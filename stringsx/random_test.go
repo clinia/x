b1fe39504c1da180afcb69643639739a5c959195
Pascale <91494180+pascalesy@users.noreply.github.com>
2024-02-05 14:32:24 -0500

    feat: rand string (#23)

---
+++ stringsx/random_test.go
@@ -0,0 +1,30 @@
+package stringsx
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRandom(t *testing.T) {
+
+	for k, tc := range []struct {
+		length int
+	}{
+		{
+			length: 0,
+		},
+		{
+			length: 5,
+		},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			str := Random(tc.length)
+			assert.Len(t, str, tc.length)
+			for i := 0; i < tc.length; i++ {
+				assert.Contains(t, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", string(str[i]))
+			}
+		})
+	}
+}
