8461636884470f4bda6c713b6a84b26d88e09fd5
CHANSIN Jonathan <jonathan.chansin@clinia.com>
2024-11-13 14:37:26 -0500

    chore: remove saramax from otelx

--- otelx/instrumentation/otelsaramax/consumer_info.go
+++
@@ -1,9 +0,0 @@
-package otelsaramax
-
-import "go.opentelemetry.io/otel/attribute"
-
-// ConsumerInfo holds information about the consumer group.
-type ConsumerInfo struct {
-	ConsumerGroup string
-	Attributes    []attribute.KeyValue
-}
