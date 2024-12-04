f96dbe3238c3847a59a95e11cbceedda8685f269
Philippe Turner <philippe.turner@clinia.com>
2024-12-04 17:02:53 -0500

    feat: Add RequestIdHook that adds the RequestId from the context to the logger fields

---
+++ logrusx/hooks.go
@@ -0,0 +1,41 @@
+package logrusx
+
+import "github.com/sirupsen/logrus"
+
+type requestIdHook struct {
+	rIdCtxKey   interface{}
+	rIdFieldKey string
+}
+
+var _ logrus.Hook = (*requestIdHook)(nil)
+
+func NewRequestIdHook(requestIdContextKey interface{}, requestIdFieldKey string) *requestIdHook {
+	return &requestIdHook{
+		rIdCtxKey:   requestIdContextKey,
+		rIdFieldKey: requestIdFieldKey,
+	}
+}
+
+func (irh *requestIdHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (rih *requestIdHook) Fire(entry *logrus.Entry) (outErr error) {
+	defer func() {
+		// Nullify panic to prevent having this hook break a request
+		if rec := recover(); rec != nil {
+			outErr = nil
+		}
+	}()
+	if entry == nil || entry.Context == nil {
+		return nil
+	}
+	requestId := entry.Context.Value(rih.rIdCtxKey)
+	if requestId == nil {
+		return nil
+	} else {
+		entry.Data[rih.rIdFieldKey] = requestId
+	}
+
+	return nil
+}
