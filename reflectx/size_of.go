942dc88314b8eec938c029297ff65b69597595e9
William Bergeron-Drouin <william.bergeron-drouin@clinia.ca>
2024-12-18 14:59:07 -0500

    Add CalculateSize util

---
+++ reflectx/size_of.go
@@ -0,0 +1,64 @@
+package reflectx
+
+import (
+	"reflect"
+	"unsafe"
+
+	"github.com/clinia/x/logrusx"
+	"github.com/clinia/x/tracex"
+)
+
+// CalculateSize calculates the size of the given value `v`.
+// This function cannot panic as it will recover and log the stack trace
+// using the provided logger `l` whenever a panic occurs.
+//
+// Parameters:
+//   - l: A logger instance used for logging stack traces in case of a panic.
+//   - v: The value whose size is to be calculated.
+//
+// Returns:
+//
+//	The size of the given value `v` as an integer.
+func CalculateSize(l *logrusx.Logger, v interface{}) int {
+	defer tracex.RecoverWithStackTracef(l, "failed to calculate size of %T", v)
+	val := reflect.ValueOf(v)
+	return sizeOfValue(val, 0)
+}
+
+func sizeOfValue(val reflect.Value, depth int) int {
+	if depth > 15 {
+		// to avoid infinite recursion
+		return 0
+	}
+
+	switch val.Kind() {
+	case reflect.Ptr:
+		if val.IsNil() {
+			return 0
+		}
+		return int(unsafe.Sizeof(uintptr(0))) + sizeOfValue(val.Elem(), depth+1)
+	case reflect.Slice:
+		size := int(unsafe.Sizeof(uintptr(0))) // size of slice header
+		for i := 0; i < val.Len(); i++ {
+			size += sizeOfValue(val.Index(i), depth+1)
+		}
+		return size
+	case reflect.Map:
+		size := int(unsafe.Sizeof(uintptr(0))) // size of map header
+		for _, key := range val.MapKeys() {
+			size += sizeOfValue(key, depth+1)
+			size += sizeOfValue(val.MapIndex(key), depth+1)
+		}
+		return size
+	case reflect.Struct:
+		size := 0
+		for i := 0; i < val.NumField(); i++ {
+			size += sizeOfValue(val.Field(i), depth+1)
+		}
+		return size
+	case reflect.String:
+		return int(unsafe.Sizeof("")) + val.Len()
+	default:
+		return int(unsafe.Sizeof(val.Interface()))
+	}
+}
---
+++ reflectx/size_of_test.go
@@ -0,0 +1,69 @@
+package reflectx
+
+import (
+	"testing"
+
+	"github.com/clinia/x/logrusx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSizeOf(t *testing.T) {
+	l := logrusx.New("", "")
+
+	type myOtherStruct struct {
+		A    int
+		B    string
+		Data []byte
+	}
+
+	type myStruct struct {
+		A      int
+		B      string
+		Data1  []byte
+		Nested *myOtherStruct
+		Others []*myOtherStruct
+	}
+
+	t.Run("should give the approximate size of a struct", func(t *testing.T) {
+		data := make([]byte, 1*1024*1024) // 1MB
+
+		mockedStruct := myStruct{
+			A:     1,
+			B:     "hello",
+			Data1: data,
+			Nested: &myOtherStruct{
+				A:    2,
+				B:    "world",
+				Data: data,
+			},
+			Others: []*myOtherStruct{
+				{
+					A:    3,
+					B:    "foo",
+					Data: data,
+				},
+				{
+					A:    4,
+					B:    "bar",
+					Data: data,
+				},
+			},
+		}
+
+		size := CalculateSize(l, mockedStruct)
+		t.Logf("Size of myStruct: %d bytes", size)
+		require.GreaterOrEqual(t, size, 4*1024*1024) // 4MB
+	})
+
+	t.Run("should avoid infinite recursion", func(t *testing.T) {
+		type infiniteRecursionStruct struct {
+			Next *infiniteRecursionStruct
+		}
+
+		mockedStruct := &infiniteRecursionStruct{}
+		mockedStruct.Next = mockedStruct
+
+		size := CalculateSize(l, mockedStruct)
+		require.GreaterOrEqual(t, size, 0)
+	})
+}
