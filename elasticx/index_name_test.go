565247eecd19fa91e18b392ec1871f2074b8e7d5
Pascale Sylvestre <91494180+pascalesy@users.noreply.github.com>
2025-01-07 15:01:51 -0500

    fix: index name methods (#81)
    * fix: index name
    
    * test: index name
    
    * refactor private methods

--- elasticx/index_name_test.go
+++ elasticx/index_name_test.go
@@ -11,4 +11,29 @@ func TestIndexName(t *testing.T) {
 		name := NewIndexName("a", "b", "c")
 		assert.Equal(t, "a~b~c", name.String())
 	})
+
+	t.Run("should return the engine name", func(t *testing.T) {
+		name := NewIndexName("a")
+		assert.Equal(t, "", name.EngineName())
+
+		name = NewIndexName("a", "b")
+		assert.Equal(t, "b", name.EngineName())
+
+		name = NewIndexName("a", "b", "c")
+		assert.Equal(t, "b", name.EngineName())
+	})
+
+	t.Run("should return the index name", func(t *testing.T) {
+		name := NewIndexName("a")
+		assert.Equal(t, "", name.Name())
+
+		name = NewIndexName("a", "b")
+		assert.Equal(t, "", name.Name())
+
+		name = NewIndexName("a", "b", "c")
+		assert.Equal(t, "c", name.Name())
+
+		name = NewIndexName("a", "b", "c", "d")
+		assert.Equal(t, "c~d", name.Name())
+	})
 }
