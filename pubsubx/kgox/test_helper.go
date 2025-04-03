5086d02a1b0eeacd9d982011dfdb627342a93b69
Pier-Olivier Beaulieu <pier-olivier.beaulieu@clinia.com>
2025-04-03 11:00:49 -0400

    chore: linting

--- pubsubx/kgox/test_helper.go
+++ pubsubx/kgox/test_helper.go
@@ -126,6 +126,7 @@ func createTopic(t *testing.T, conf *pubsubx.Config, topic messagex.Topic) {
 	admCl := getAdmCl()
 	defer admCl.Close()
 	// Create the topic
+	//nolint:all
 	res, err := admCl.CreateTopics(context.Background(), 1, int16(len(conf.Providers.Kafka.Brokers)), map[string]*string{}, topic.TopicName(conf.Scope))
 	require.NoError(t, err, "failed to create topic '%s'", topic.TopicName(conf.Scope))
 	require.NoError(t, res.Error(), "failed to create topic '%s'", topic.TopicName(conf.Scope))
