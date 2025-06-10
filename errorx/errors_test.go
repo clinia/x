1270f96491772a50c54326e27198c1ad53206899
philippeturner <philippe.turner@clinia.com>
2025-06-10 13:41:37 -0400

    fix

--- errorx/errors_test.go
+++ errorx/errors_test.go
@@ -8,18 +8,24 @@ import (
 )
 
 func TestOutputErrsMatchInputLength(t *testing.T) {
-	t.Run("should return an error on length mismatch with proper error format (InternalError)", func(t *testing.T) {
-		err := OutputErrsMatchInputLength(0, 2, InternalErrorf)
+	t.Run("should return an error on length mismatch", func(t *testing.T) {
+		err := OutputErrsMatchInputLength(0, 2, nil)
 		require.Error(t, err)
 		assert.Equal(t, "[INTERNAL] a different length of errors (0) then the input length (2) was returned", err.Error())
 	})
 
-	t.Run("should return an error on length mismatch with proper error format (InvalidArgumentError)", func(t *testing.T) {
-		err := OutputErrsMatchInputLength(0, 2, InvalidArgumentErrorf)
+	t.Run("should return an error on length mismatch with error", func(t *testing.T) {
+		err := OutputErrsMatchInputLength(0, 2, InvalidArgumentErrorf("test error"))
 		require.Error(t, err)
-		assert.Equal(t, "[INVALID_ARGUMENT] a different length of errors (0) then the input length (2) was returned", err.Error())
+		assert.Equal(t, "[INVALID_ARGUMENT] test error", err.Error())
 	})
 	t.Run("should return no error on length match", func(t *testing.T) {
-		assert.NoError(t, OutputErrsMatchInputLength(2, 2, InternalErrorf))
+		assert.NoError(t, OutputErrsMatchInputLength(2, 2, nil))
+	})
+
+	t.Run("should return error on length match with error", func(t *testing.T) {
+		err := OutputErrsMatchInputLength(2, 2, InvalidArgumentErrorf("test error"))
+		require.Error(t, err)
+		assert.Equal(t, "[INVALID_ARGUMENT] test error", err.Error())
 	})
 }
