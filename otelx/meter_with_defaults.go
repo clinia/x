cf1627704b562952550497d8cb30e8f8519e8f63
William Bergeron-Drouin <william.bergeron-drouin@clinia.com>
2026-03-11 10:09:28 -0400

    Add instruments with defaults

---
+++ otelx/meter_with_defaults.go
@@ -0,0 +1,36 @@
+package otelx
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/metric"
+)
+
+type (
+	numericInstrumentType interface {
+		int64 | float64
+	}
+	nInstrument[N numericInstrumentType] interface {
+		Record(ctx context.Context, incr N, options ...metric.RecordOption)
+	}
+	instrumentWithDefaults[N numericInstrumentType, T nInstrument[N]] struct {
+		t           T
+		defaultOpts []metric.RecordOption
+	}
+)
+
+var (
+	_ nInstrument[int64] = (*instrumentWithDefaults[int64, nInstrument[int64]])(nil)
+)
+
+func NewInstrumentWithDefaults[N numericInstrumentType, T nInstrument[N]](t T, defaults ...metric.RecordOption) *instrumentWithDefaults[N, T] {
+	return &instrumentWithDefaults[N, T]{
+		t:           t,
+		defaultOpts: defaults,
+	}
+}
+
+// Record implements [nInstrument].
+func (i *instrumentWithDefaults[N, T]) Record(ctx context.Context, incr N, options ...metric.RecordOption) {
+	i.t.Record(ctx, incr, append(i.defaultOpts, options...)...)
+}
