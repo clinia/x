3749bfb5905339f208a782d9b0cf9f4b8a15ae0c
William Bergeron-Drouin <william.bergeron-drouin@clinia.com>
2026-03-13 06:49:09 -0400

    Change generic type name

--- otelx/meter_with_defaults.go
+++ otelx/meter_with_defaults.go
@@ -15,8 +15,8 @@ type (
 	nInstrument[N numericInstrumentType] interface {
 		Record(ctx context.Context, incr N, options ...metric.RecordOption)
 	}
-	InstrumentWithDefaults[N numericInstrumentType, T nInstrument[N]] struct {
-		t           T
+	InstrumentWithDefaults[N numericInstrumentType, InstType nInstrument[N]] struct {
+		t           InstType
 		defaultOpts []metric.RecordOption
 	}
 
@@ -24,8 +24,8 @@ type (
 		Record(ctx context.Context, val N, systemName dbconv.SystemNameAttr, attrs ...attribute.KeyValue)
 	}
 
-	InstrumentWithDBSystemAndDefaults[N numericInstrumentType, T nInstrumentWithDBSystem[N]] struct {
-		t            T
+	InstrumentWithDBSystemAndDefaults[N numericInstrumentType, InstType nInstrumentWithDBSystem[N]] struct {
+		t            InstType
 		defaultAttrs []attribute.KeyValue
 	}
 )
@@ -37,26 +37,26 @@ var (
 	_ nInstrumentWithDBSystem[float64] = (*InstrumentWithDBSystemAndDefaults[float64, nInstrumentWithDBSystem[float64]])(nil)
 )
 
-func NewInstrumentWithDefaults[N numericInstrumentType, T nInstrument[N]](t T, defaults ...metric.RecordOption) *InstrumentWithDefaults[N, T] {
-	return &InstrumentWithDefaults[N, T]{
+func NewInstrumentWithDefaults[N numericInstrumentType, InstType nInstrument[N]](t InstType, defaults ...metric.RecordOption) *InstrumentWithDefaults[N, InstType] {
+	return &InstrumentWithDefaults[N, InstType]{
 		t:           t,
 		defaultOpts: defaults,
 	}
 }
 
 // Record implements [nInstrument].
-func (i *InstrumentWithDefaults[N, T]) Record(ctx context.Context, incr N, options ...metric.RecordOption) {
+func (i *InstrumentWithDefaults[N, InstType]) Record(ctx context.Context, incr N, options ...metric.RecordOption) {
 	i.t.Record(ctx, incr, append(i.defaultOpts, options...)...)
 }
 
-func NewInstrumentWithDBSystemAndDefaults[N int64 | float64, T nInstrumentWithDBSystem[N]](t T, defaultAttrs ...attribute.KeyValue) *InstrumentWithDBSystemAndDefaults[N, T] {
-	return &InstrumentWithDBSystemAndDefaults[N, T]{
+func NewInstrumentWithDBSystemAndDefaults[N int64 | float64, InstType nInstrumentWithDBSystem[N]](t InstType, defaultAttrs ...attribute.KeyValue) *InstrumentWithDBSystemAndDefaults[N, InstType] {
+	return &InstrumentWithDBSystemAndDefaults[N, InstType]{
 		t:            t,
 		defaultAttrs: defaultAttrs,
 	}
 }
 
 // Record implements [nInstrumentWithDBSystem].
-func (i *InstrumentWithDBSystemAndDefaults[N, T]) Record(ctx context.Context, val N, systemName dbconv.SystemNameAttr, attrs ...attribute.KeyValue) {
+func (i *InstrumentWithDBSystemAndDefaults[N, InstType]) Record(ctx context.Context, val N, systemName dbconv.SystemNameAttr, attrs ...attribute.KeyValue) {
 	i.t.Record(ctx, val, systemName, append(i.defaultAttrs, attrs...)...)
 }
